99f27d08a9497ec97902d720aa1151413d1f69ab
DigiConvent <129603111+DigiConvent@users.noreply.github.com>
2024-10-28 18:40:54 +0100

    Update 01_init.sql

--- backend/data/db/migrations/0.0.0/01_init.sql
+++ backend/data/db/migrations/0.0.0/01_init.sql
@@ -1,11 +1,11 @@
-create table if not exists versions (
+create table versions (
   id serial primary key,
   major int not null,
   minor int not null,
   patch int not null
 );
 
-create table if not exists test_table (
+create table test_table (
   id serial primary key,
   name text not null
 );
