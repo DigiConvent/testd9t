86c3473b463fe74fddd955cdef50d3cb75c0939d
digiconvent <digiconvent1815@gmail.com>
2025-02-01 00:28:19 +0100

    fix letsencrypt script

--- install/ubuntu/do_create_letsencrypt_certs.sh
+++ install/ubuntu/do_create_letsencrypt_certs.sh
@@ -10,6 +10,6 @@ sudo certbot certonly --standalone -d "$domain" --register-unsafely-without-emai
 
 sudo mkdir -p /home/digiconvent/certs
 
-sudo cp /etc/letsencrypt/live/digiconvent.de/fullchain.pem /home/digiconvent/certs/
-sudo cp /etc/letsencrypt/live/digiconvent.de/privkey.pem /home/digiconvent/certs/
+sudo cp /etc/letsencrypt/live/$domain/fullchain.pem /home/digiconvent/certs/
+sudo cp /etc/letsencrypt/live/$domain/privkey.pem /home/digiconvent/certs/
 chmod 777 /etc/digiconvent/certs/*
