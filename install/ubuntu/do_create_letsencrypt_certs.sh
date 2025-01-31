a69d71ddadc06d8d6f8b9c50277f1978c02966a7
digiconvent <digiconvent1815@gmail.com>
2025-02-01 00:17:06 +0100

    fix letsencrypt script

--- install/ubuntu/do_create_letsencrypt_certs.sh
+++ install/ubuntu/do_create_letsencrypt_certs.sh
@@ -2,7 +2,13 @@
 
 sudo apt-get install -y certbot python3-certbot-nginx
 
-certbot certonly --standalone -d $1 --register-unsafely-without-email --non-interactive --agree-tos
+domain=$1
+
+echo "Creating letsencrypt certificates for $domain"
+
+sudo certbot certonly --standalone -d "$domain" --register-unsafely-without-email --non-interactive --agree-tos
+
+sudo mkdir -p /home/digiconvent/certs
 
 sudo cp /etc/letsencrypt/live/digiconvent.de/fullchain.pem /home/digiconvent/certs/
 sudo cp /etc/letsencrypt/live/digiconvent.de/privkey.pem /home/digiconvent/certs/
