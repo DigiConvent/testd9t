e09829c7717e5575c06e133f9d0f6c967f6edb24
digiconvent <digiconvent1815@gmail.com>
2025-02-03 05:56:21 +0100

    fix paths

--- backend/pkg/sys/service/service.artifacts.install.go
+++ backend/pkg/sys/service/service.artifacts.install.go
@@ -15,23 +15,24 @@ import (
 func (s *SysService) InstallArtifacts(tag *sys_domain.ReleaseTag) *core.Status {
 	log.Info("Installing artifacts for version " + tag.Tag)
 	var err error
-	homeFolder := "/home/testd9t"
-	err = tag.DownloadAsset("main", homeFolder+"/backend/main")
+	homeFolder := "/home/testd9t/"
+	tmpFolder := "/tmp/testd9t/"
+	err = tag.DownloadAsset("main", homeFolder+"backend/main")
 	if err != nil {
 		return core.InternalError("Error downloading binary for version: " + tag.Tag + " " + err.Error())
 	}
 
-	err = tag.DownloadAsset("frontend.zip", "/tmp/frontend.zip")
+	err = tag.DownloadAsset("frontend.zip", tmpFolder+"frontend.zip")
 	if err != nil {
 		return core.InternalError("Error downloading frontend for version: " + tag.Tag + " " + err.Error())
 	}
 
-	err = exec.Command("chmod", "+x", homeFolder+"/backend/main").Run()
+	err = exec.Command("chmod", "+x", homeFolder+"backend/main").Run()
 	if err != nil {
 		return core.InternalError("Error setting permissions for binary: " + err.Error())
 	}
 
-	readClose, err := zip.OpenReader("/tmp/frontend.zip")
+	readClose, err := zip.OpenReader(tmpFolder + "frontend.zip")
 	if err != nil {
 		return core.InternalError("Error opening frontend.zip: " + err.Error())
 	}
@@ -39,7 +40,7 @@ func (s *SysService) InstallArtifacts(tag *sys_domain.ReleaseTag) *core.Status {
 	for _, f := range readClose.File {
 		name, _ := strings.CutPrefix(f.Name, "frontend/dist/")
 		if f.FileInfo().IsDir() {
-			err := os.MkdirAll("/tmp/testd9t/frontend/"+name, os.ModePerm)
+			err := os.MkdirAll(homeFolder+"frontend/"+name, os.ModePerm)
 			if err != nil {
 				return core.InternalError("Error creating directory:" + err.Error())
 			}
@@ -47,7 +48,7 @@ func (s *SysService) InstallArtifacts(tag *sys_domain.ReleaseTag) *core.Status {
 		}
 		reader, _ := f.OpenRaw()
 
-		file, err := os.Create("/home/testd9t/frontend/" + name)
+		file, err := os.Create(homeFolder + "frontend/" + name)
 		if err != nil {
 			return core.InternalError("Error creating file " + file.Name() + err.Error())
 		}
