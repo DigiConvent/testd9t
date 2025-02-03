03e87a59376f70a195fe36cfe39ab3754f9268a5
digiconvent <digiconvent1815@gmail.com>
2025-02-03 14:34:09 +0100

    fix cert paths

--- install/arch/do_create_letsencrypt_certs.sh
+++ install/arch/do_create_letsencrypt_certs.sh
@@ -7,10 +7,10 @@ domain="$1"
 echo "Creating letsencrypt certificates for $domain"
 
 sudo mkdir -p /home/testd9t/certs
+rm /home/testd9t/certs/*
 
 sudo certbot certonly --webroot -w /home/testd9t/certs -d "$domain"
 
-rm /home/testd9t/certs/*
 
 sudo cp /etc/letsencrypt/live/$domain/fullchain.pem /home/testd9t/certs/
 sudo cp /etc/letsencrypt/live/$domain/privkey.pem /home/testd9t/certs/
