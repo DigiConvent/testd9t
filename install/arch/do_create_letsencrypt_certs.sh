bdca42232d28cc20f27af5e1b1c8faff466c390d
digiconvent <digiconvent1815@gmail.com>
2025-02-03 14:19:31 +0100

    correct the cert creation scripts

--- install/arch/do_create_letsencrypt_certs.sh
+++ install/arch/do_create_letsencrypt_certs.sh
@@ -6,12 +6,13 @@ domain="$1"
 
 echo "Creating letsencrypt certificates for $domain"
 
-sudo certbot certonly --standalone -d "$domain" --register-unsafely-without-email --non-interactive --agree-tos
-
 sudo mkdir -p /home/testd9t/certs
+
+sudo certbot certonly --webroot -w /home/testd9t/certs -d "$domain"
 
 rm /home/testd9t/certs/*
 
 sudo cp /etc/letsencrypt/live/$domain/fullchain.pem /home/testd9t/certs/
 sudo cp /etc/letsencrypt/live/$domain/privkey.pem /home/testd9t/certs/
+
 sudo chown -R testd9t:testd9t /home/testd9t/certs
