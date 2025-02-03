d8e4b66609290b3fddb6663cc652f3eea8c556bb
digiconvent <digiconvent1815@gmail.com>
2025-02-03 03:49:27 +0100

    add update db to program version

--- backend/cli/migrate.go
+++ backend/cli/migrate.go
@@ -2,26 +2,11 @@ package cli
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/DigiConvent/testd9t/core/db"
 	sys_service "github.com/DigiConvent/testd9t/pkg/sys/service"
 )
 
 func MigrateDB(sysService sys_service.SysServiceInterface) {
 	fmt.Println("--migrate-db")
-	sysStatus, status := sysService.GetSystemStatus()
-
-	if status.Err() {
-		fmt.Println("Error getting system status:", status.Message)
-		os.Exit(1)
-	}
-	packages := db.ListPackages()
-
-	for _, pkg := range packages {
-		status := sysService.MigratePackage(pkg, sysStatus.ProgramVersion)
-		if status.Err() && status.Code != 404 {
-			fmt.Println("Error migrating package", pkg, ":", status.Message)
-		}
-	}
+	sysService.MigrateDatabase(nil)
 }
