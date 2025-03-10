94fee565d683ff87354e11eb9998c15014abb73c
digiconvent <digiconvent1815@gmail.com>
2025-03-11 00:29:31 +0100

    fix api endpoint access url

--- frontend/src/api/iam/user/connect_telegram.ts
+++ frontend/src/api/iam/user/connect_telegram.ts
@@ -2,5 +2,5 @@ import type Either from "@/api/core/either";
 import { api_post } from "@/api/core/fetch";
 
 export async function connect_telegram(init_data: string) : Promise<Either<string, boolean>> {
-   return api_post<boolean>("/api/iam/auth/telegram/connect", { payload: init_data }, undefined, 200);
+   return api_post<boolean>("/api/iam/login/telegram/connect", { payload: init_data }, undefined, 200);
 }
