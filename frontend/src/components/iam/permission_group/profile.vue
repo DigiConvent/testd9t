b2cf6c63e9e04441e381ba4408c1f322a56f9a72
digiconvent <digiconvent1815@gmail.com>
2025-03-29 18:33:35 +0100

    fix nextSibling err

--- frontend/src/components/iam/permission_group/profile.vue
+++ frontend/src/components/iam/permission_group/profile.vue
@@ -1,59 +1,67 @@
 <template>
-   <ProgressBar v-if="loading" mode="indeterminate"></ProgressBar>
-   <NeedsPermission v-else-if="profile" permission="iam.permission_group.read">
-      <div class="grid grid-cols-2 gap-4">
-         <Card>
-            <template #title> {{ $t("iam.pg.profile.hierarchy") }} </template>
-            <template #content>
-               <Timeline :value="ancestors">
-                  <template #content="slotProps">
-                     <router-link
-                        :to="{ name: 'iam.pg.profile', params: { id: slotProps.item.id } }"
-                        >{{ slotProps.item.name }}</router-link
-                     >
-                  </template>
-               </Timeline>
-            </template>
-         </Card>
-         <Card>
-            <template #title>{{ $t("iam.pg.profile.permissions") }}</template>
-            <template #content>
-               <div class="grid grid-cols-2">
-                  <div>
-                     {{ $t("iam.pg.profile.inherited") }}
-                     <div v-for="p of profile?.permissions.filter((p) => p.implied)" :key="p.name">
-                        {{ p.name }}
+   <div>
+      <ProgressBar v-if="loading" mode="indeterminate"></ProgressBar>
+      <NeedsPermission v-else-if="profile" permission="iam.permission_group.read">
+         <div class="grid grid-cols-2 gap-4">
+            <Card>
+               <template #title> {{ $t("iam.pg.profile.hierarchy") }} </template>
+               <template #content>
+                  <Timeline :value="ancestors">
+                     <template #content="slotProps">
+                        <router-link
+                           :to="{ name: 'iam.pg.profile', params: { id: slotProps.item.id } }"
+                           >{{ slotProps.item.name }}</router-link
+                        >
+                     </template>
+                  </Timeline>
+               </template>
+            </Card>
+            <Card>
+               <template #title>{{ $t("iam.pg.profile.permissions") }}</template>
+               <template #content>
+                  <div class="grid grid-cols-2">
+                     <div>
+                        {{ $t("iam.pg.profile.inherited") }}
+                        <div
+                           v-for="p of profile?.permissions.filter((p) => p.implied)"
+                           :key="p.name"
+                        >
+                           {{ p.name }}
+                        </div>
+                     </div>
+                     <div>
+                        {{ $t("iam.pg.profile.owned") }}
+                        <div
+                           v-for="p of profile?.permissions.filter((p) => !p.implied)"
+                           :key="p.name"
+                        >
+                           {{ p.name }}
+                        </div>
                      </div>
                   </div>
-                  <div>
-                     {{ $t("iam.pg.profile.owned") }}
-                     <div v-for="p of profile?.permissions.filter((p) => !p.implied)" :key="p.name">
-                        {{ p.name }}
-                     </div>
-                  </div>
-               </div>
-            </template>
-         </Card>
-         <Card>
-            <template #title>{{ $t("iam.pg.profile.properties") }}</template>
-            <template #content>
-               <UpdatePermissionGroup
-                  v-model="profile!.permission_group.id"
-               ></UpdatePermissionGroup>
-            </template>
-         </Card>
-         <Card>
-            <template #title>{{ $t("iam.pg.profile.members") }}</template>
-            <template #content>
-               <UserFacades v-if="profile.members.length > 0" :users="profile!.members" />
-               <span v-else>
-                  {{ $t("iam.pg.profile.no_members") }}
-               </span>
-            </template>
-         </Card>
-      </div>
-   </NeedsPermission>
-   <div v-else>Something went wrong loading this permission group</div>
+               </template>
+            </Card>
+            <Card>
+               <template #title>{{ $t("iam.pg.profile.properties") }}</template>
+               <template #content>
+                  <UpdatePermissionGroup
+                     v-model="profile!.permission_group.id"
+                  ></UpdatePermissionGroup>
+               </template>
+            </Card>
+            <Card>
+               <template #title>{{ $t("iam.pg.profile.members") }}</template>
+               <template #content>
+                  <UserFacades v-if="profile.members.length > 0" :users="profile!.members" />
+                  <span v-else>
+                     {{ $t("iam.pg.profile.no_members") }}
+                  </span>
+               </template>
+            </Card>
+         </div>
+      </NeedsPermission>
+      <div v-else>Something went wrong loading this permission group</div>
+   </div>
 </template>
 
 <script lang="ts" setup>
